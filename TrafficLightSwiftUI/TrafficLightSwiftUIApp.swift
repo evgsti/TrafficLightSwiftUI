e143e8505c1d90fab37ecf814310a6addc79bd04
Evgeniy <ev.shipitsyn@yandex.ru>
2024-10-07 18:14:38 +0300

    Initial Commit

---
+++ TrafficLightSwiftUI/TrafficLightSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSwiftUIApp.swift
+//  TrafficLightSwiftUI
+//
+//  Created by Евгений on 07.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
